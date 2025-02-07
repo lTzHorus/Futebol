7f95a4b000c13dfac20dc1a6f777e7923927e6a0
lTzHorus <leo_imarques@hotmail.com>
2025-02-07 14:58:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 // This script can be expanded for future functionality
 document.addEventListener('DOMContentLoaded', () => {
     const membersData = [
-        { name: 'Leonardo', status: 'paid', amount: 54 },
+        { name: 'Leonardo', status: 'paid', amount: 56 },
         { name: 'Luiz', status: 'paid', amount: 43 },
         { name: 'Jhoww', status: 'paid', amount: 43 },
         { name: 'Lucas', status: 'paid', amount: 43 },
         { name: 'Hiago', status: 'paid', amount: 43 },
         { name: 'Caio', status: 'paid', amount: 43 },
-        { name: 'Lopes', status: 'pending-next', amount: 0 },
+        { name: 'Lopes', status: 'paid', amount: 43 },
         { name: 'Danilo', status: 'paid', amount: 43 },
         { name: 'Paulo', status: 'paid', amount: 43 },
         { name: 'Gustavo', status: 'paid', amount: 43 },
         { name: 'Mike', status: 'paid', amount: 43 },
-        { name: 'Will', status: 'pending-today', amount: 0 },
+        { name: 'Will', status: 'paid', amount: 43 },
         { name: 'Yago', status: 'paid', amount: 43 },
         { name: 'Eduardo', status: 'paid', amount: 43 },
         { name: 'Gui', status: 'paid', amount: 43 },
         { name: 'Renan', status: 'paid', amount: 43 },
-        { name: 'Daniel', status: 'pending-today', amount: 43 },
-        { name: 'Axel', status: 'paid', amount: 43 }
+        { name: 'Axel', status: 'paid', amount: 43 },
+        { name: 'Mauricio', status: 'paid', amount: 43 },
+        { name: 'Elielton', status: 'pending-today', amount: 43 }
     ];
 
     const tableBody = document.querySelector('#membersTable tbody');
